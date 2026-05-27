e0e4799741474e39b4cfd4eec77360894b7ef8ba
prashantdagal03 <prashantdagal03@gmail.com>
2026-05-28 00:49:19 +0530

    Update DB config for Render deployment

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'mini crm',
-  password:  '12345678',
-  port: 5432,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'mini crm',
+    password: '12345678',
+    port: 5432,
 });
 
 module.exports = pool;
